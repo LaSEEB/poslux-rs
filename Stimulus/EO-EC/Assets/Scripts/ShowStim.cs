1a318b928b73c3c92492b9d066c124f53df663c0
Athanasios (Thanos) Vourvopoulos <a.vourvopoulos@gmail.com>
2021-03-28 11:48:09 +0100

    audio fixed

--- Stimulus/EO-EC/Assets/Scripts/ShowStim.cs
+++ Stimulus/EO-EC/Assets/Scripts/ShowStim.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 using Assets.LSL4Unity.Scripts.AbstractInlets;
 using Assets.LSL4Unity.Scripts.Examples;
 
+[RequireComponent(typeof(AudioSource))]
 public class ShowStim : MonoBehaviour {
 
     public Text cross, eo, ec, thend;
 
-    //public GameObject EndofSessionPanel;
-
-    //public GameObject lslobject;
+    AudioSource audioData;
 
     // Use this for initialization
     void Start()
     {
 
+        audioData = GetComponent<AudioSource>();
+        audioData.Pause();
+
         cross.enabled = true;
         eo.enabled = false;
         ec.enabled = false;
         thend.enabled = false;
-        //EndofSessionPanel.SetActive(false);
 
         Cursor.visible = false;
     }
@@ -30,7 +31,6 @@ public class ShowStim : MonoBehaviour {
     void Update()
     {
 
-
         //quit 
         if (Input.GetKey("escape"))
         {
@@ -38,20 +38,22 @@ public class ShowStim : MonoBehaviour {
             Application.Quit();
         }
 
-        //reload scene
-        //		if (Input.GetKey(KeyCode.R))
-        //		{
-        //			ReloadScene();
-        //		}
-
-
         getStim();
-
-     //   if (ReceiveLSL.getLSLsample() == 1010) //32770 experiment stop
-     //       EndofSessionPanel.SetActive(true); //pop window
 
     }
 
+    void beep(bool state) {
+        if (state && flipswitch) {
+            audioData.Play(0);
+            flipswitch = false;
+        }
+    }
+    private bool fswitch; 
+    public bool flipswitch 
+    {
+        get { return fswitch; }   // get
+        set { fswitch = value; }  // set
+    }
 
     void getStim()
     {
@@ -60,37 +62,44 @@ public class ShowStim : MonoBehaviour {
         switch (stim)
         {
             case 768: //hide cross
+                flipswitch = true; //
                 cross.enabled = false;
                 eo.enabled = false;
                 ec.enabled = false;
                 thend.enabled = false;
                 break;
             case 800: //hide cross
+                flipswitch = true; //
                 cross.enabled = false;
                 eo.enabled = false;
                 ec.enabled = false;
                 break;
             case 786: // show cross
+                flipswitch = true; //
                 cross.enabled = true;
                 eo.enabled = false;
                 ec.enabled = false;
                 break;
             case 7700: //eyes closed
+                beep(true);
                 cross.enabled = false;
                 eo.enabled = false;
                 ec.enabled = true;
                 break;
             case 7690: // eyes open
+                beep(true);
                 cross.enabled = false;
                 eo.enabled = true;
                 ec.enabled = false;
                 break;
             case 781: // hide arrow
+                flipswitch = true; //
                 cross.enabled = true;
                 eo.enabled = false;
                 ec.enabled = false;
                 break;
             case 1010: // hide arrow
+                flipswitch = true; //
                 cross.enabled = false;
                 eo.enabled = false;
                 ec.enabled = false;
