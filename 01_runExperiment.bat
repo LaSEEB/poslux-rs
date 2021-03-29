8eb78dde6b631d53779aa3692ec06b506c9b7226
Athanasios (Thanos) Vourvopoulos <8280871+athanoid@users.noreply.github.com>
2021-03-29 14:17:43 +0100

    added RDA

--- 01_runExperiment.bat
+++ 01_runExperiment.bat
@@ -2,4 +2,4 @@
 set mypath=%cd%
 START %mypath%\Stimulus\EO-EC\Builds\Builds.exe
 TIMEOUT 5
-neuxus %mypath%\Stimulus\generate_stim.py
+neuxus %mypath%\Stimulus\acquire_data.py
--- Stimulus/acquire_data.py
+++ Stimulus/acquire_data.py
@@ -5,18 +5,30 @@ from neuxus.nodes import *
 #    value='EEG',
 #    data_type='signal'
 #)
+# receive signals from BrainProducts Recorder via RDA
+rda_receive = io.RdaReceive(
+    rdaport=51244,
+    offset=.0,
+    host='localhost')
 
 # dir_path is the path to directory containing this file
 stim_cfg = dir_path + '/stimulations.xml'
 
-# Node 1
+# Generate stimulations
 generated_markers = stimulator.Stimulator(
     file=stim_cfg
 )
 
-
-lsl_send = io.LslSend(
+# send markers via LSL
+lsl_markersd = io.LslSend(
     input_port=generated_markers.output,
     type="marker",
     name='eoec_markers'
+)
+
+# send RDA signals via LSL
+lsl_signal = io.LslSend(
+    input_port=rda_receive.output,
+    type="EEG",
+    name='eoec_signal'
 )
